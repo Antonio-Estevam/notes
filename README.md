b53cb299a9373ca1499b1abfd3b162c501a7bb9e
antonio-estevam <jhonnyimmbe@gmail.com>
2024-02-08 15:41:33 -0300

    adaptation to mobile devices, delete note, audio typing

---
+++ README.md
@@ -0,0 +1,19 @@
+![Cover](./.github/cover.png)
+
+![alt](./.github/Logo-nlw.svg)
+
+This application was developed using React, TypeScript, Tailwind and the SpeechRecognition API.<br/>
+Essa aplicação foi desenvolvida utilizando React, TypeScript, Tailwind e a SpeechRecognition API.
+
+## Running | Executando
+
+After cloning the repository, access the project folder and execute the commands below:<br/>
+Após clonar o repositório, acesse a pasta do projeto e execute os comandos abaixo:
+
+```sh
+npm install
+npm run dev
+```
+
+Visit http://localhost:5173 to view the application.<br/>
+Acesse http://localhost:5173 para visualizar a aplicação.
--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,60 @@
+import { ChangeEvent, useState } from "react"
 import logo from "./assets/Logo-nlw.svg"
 import { NewNoteCard } from "./components/new-note-card"
 import { NoteCard } from "./components/note-card"
 
+interface Notes {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
 
+  const [ search, setSearch ] = useState("")
+  const [notes, setNotes] = useState<Notes[]>(() => {
+  const notesOnStorage = localStorage.getItem("notes")
+
+    if(notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  })
+
+  function onNoteCreate(content: string){
+    const newNote = {
+       id: crypto.randomUUID(),
+       date: new Date(),
+       content
+    }
+    
+    const notesArray = [newNote,...notes]
+    setNotes(notesArray)
+
+    localStorage.setItem("notes", JSON.stringify(notesArray))
+  }
+
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem("notes", JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event:ChangeEvent<HTMLInputElement>){
+    const query = event.target.value
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ""
+    ? notes.filter(notes => notes.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
+
+
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Logo nlw" />
 
       <form className="w-full">
@@ -14,18 +63,19 @@ export function App() {
         type="text" 
         placeholder="Busque em suas notas..."
         className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
-        />
+        onChange={handleSearch}
+      />
       </form> 
 
       <div className="h-px bg-slate-700" />
-        <div className="grid grid-cols-3 auto-rows-[250px] gap-6" >
-        <NewNoteCard/>
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: "Teste"
-          }}
-        />
+        <div className="grid grid-cols-1 md:grid-cols-3 auto-rows-[250px] gap-6" >
+        <NewNoteCard onNoteCreate={onNoteCreate}/>
+
+        {filteredNotes.map(note => {
+          return  <NoteCard note={note} key={note.id} onNoteDeleted={onNoteDeleted}/>
+        })}
+
+
       </div>
 
     </div>
